f21debf24754765462673a73c37f9e8760d24701
Ryan Song <ryansong612@gmail.com>
2025-02-01 22:14:08 +0000

    feat: fixed event abc

--- event.py
+++ event.py
@@ -2,40 +2,46 @@ from abc import ABC
 from typing import Tuple
 
 class Event(ABC):
-    def __init__(self, duration: int):
+    def __init__(self, duration: int, chance: float):
         self.duration = duration
+        self.chance = chance
         return
 
 class Flood(Event):
-    def __init__(self, duration: int, source: Tuple[int, int], flood_chance: float):
-        super().__init__(duration)
+    def __init__(self, duration: int, chance: float, source: Tuple[int, int]):
+        super().__init__(duration, chance)
         self.source = source
-        self.flood_chance = flood_chance
 
 class Drought(Event):
-    def __init__(self, duration: int, drought_chance: float):
-        super().__init__(duration)
-        self.drought_chance = drought_chance
+    def __init__(self, duration: int, chance: float):
+        super().__init__(duration, chance)
     
 class HeatWave(Event):
-    def __init__(self, duration: int, temperature: float, offset: int):
-        super().__init__(duration)
+    def __init__(self, duration: int, chance: float, temperature: float, offset: int):
+        super().__init__(duration, chance)
         self.temperature = temperature
         self.offset = 0
 
 class ColdSnap(Event):
-    def __init__(self, duration: int, temperature: float):
-        super().__init__(duration)
+    def __init__(self, duration: int, chance: float, temperature: float):
+        super().__init__(duration, chance)
         self.temperature = temperature
     
 class Wind(Event):
-    def __init__(self, duration: int, wind_speed: float):
+    def __init__(self, duration: int, chance: float, wind_speed: float):
         super().__init__(duration)
         self.wind_speed = wind_speed
 
 # TODO: get values from AI
 class Disease(Event):
-    def __init__(self, diffusion_coeff: float, infection_p: float = 0.25, healthy_threshold: float = 0.5, disease_threshold: float = 0.8):
+    def __init__(self, 
+                 duration: int,
+                 chance: float,
+                 diffusion_coeff: float, 
+                 infection_p: float = 0.25, 
+                 healthy_threshold: float = 0.5, 
+                 disease_threshold: float = 0.8):
+        super().__init__(duration, chance)
         self.diffusion_coeff = diffusion_coeff
         self.infection_p = infection_p # chance of randomly getting infected e.g from bugs
         self.healthy_threshold = healthy_threshold
